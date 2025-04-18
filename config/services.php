8c264e461e905d59c3b7eb5ca7f272764eccd944
Rudra Sharan <rudrasharan45@gmail.com>
2025-04-18 17:40:58 +0530

    payment method

--- config/services.php
+++ config/services.php
@@ -28,6 +28,11 @@ return [
         'key' => env('RESEND_KEY'),
     ],
 
+    'razorpay' => [
+        'key' => env('RAZORPAY_KEY'),
+        'secret' => env('RAZORPAY_SECRET'),
+    ],
+
     'slack' => [
         'notifications' => [
             'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
--- resources/views/components/input-field.blade.php
+++ resources/views/components/input-field.blade.php
@@ -1,31 +1,4 @@
-{{-- @props([
-    'type' => 'text',
-    'name',
-    'label' => null,
-    'value' => '',
-    'placeholder' => '',
-])
 
-<div class="mb-3">
-    @if($label)
-        <label for="{{ $name }}" class="block text-sm font-medium text-gray-700 mb-1">
-            {{ $label }}
-        </label>
-    @endif
-
-    <input
-        type="{{ $type }}"
-        name="{{ $name }}"
-        id="{{ $name }}"
-        value="{{ $value }}"
-        placeholder="{{ $placeholder }}"
-        {{ $attributes->merge(['class' => 'w-full border-gray-300 rounded focus:ring-purple-600 focus:border-purple-600']) }}
-    >
-
-    @error($name)
-        <p class="text-sm text-red-600 mt-1">{{ $message }}</p>
-    @enderror
-</div> --}}
 @props([
     'type' => 'text',
     'name',
