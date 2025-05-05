500fed8c032c65a763a8cc5332d30a01d5765476
Rudra Sharan <rudrasharan45@gmail.com>
2025-05-06 01:55:15 +0530

    Setup vue char

--- backend/src/components/core/Charts/Doughnut.vue
+++ backend/src/components/core/Charts/Doughnut.vue
@@ -1,87 +1,13 @@
-<!-- <script>
-import { defineComponent, h } from 'vue'
 
-import { Doughnut } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  ArcElement,
-  CategoryScale
-} from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
-
-export default defineComponent({
-  name: 'DoughnutChart',
-  components: {
-    Doughnut
-  },
-  props: {
-    chartId: {
-      type: String,
-      default: 'doughnut-chart'
-    },
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-      default: 400
-    },
-    cssClasses: {
-      default: '',
-      type: String
-    },
-    styles: {
-      type: Object,
-      default: () => {}
-    },
-    plugins: {
-      type: Array,
-      default: () => []
-    },
-    data: {
-      type: Object,
-      required: true
-    }
-  },
-  setup(props) {
-    const chartOptions = {
-      responsive: true,
-      maintainAspectRatio: false
-    }
-
-    return () =>
-      h(Doughnut, {
-        chartData: props.data,
-        chartOptions,
-        chartId: props.chartId,
-        width: props.width,
-        height: props.height,
-        cssClasses: props.cssClasses,
-        styles: props.styles,
-        plugins: props.plugins
-      })
-  }
-})
-
-</script>
-
-<style scoped>
-
-</style> -->
 
 <script setup>
 import { Doughnut } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } from 'chart.js'
 
-// Registering chart.js components
+
 ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
 
-// Data and options directly defined
+
 const data = {
   labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
   datasets: [
@@ -110,66 +36,3 @@ canvas {
 }
 </style>
 
-
-<!-- <script >
-import { defineComponent, h } from 'vue'
-import { Doughnut } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale)
-
-export default defineComponent({
-  name: 'DoughnutChart',
-  components: {
-    Doughnut
-  },
-  props: {
-    chartId: {
-      type: String,
-      default: 'doughnut-chart'
-    },
-    width: {
-      type: Number,
-      default: 400
-    },
-    height: {
-      type: Number,
-      default: 400
-    },
-    cssClasses: {
-      type: String,
-      default: ''
-    },
-    styles: {
-      type: Object,
-      default: () => {}
-    },
-    plugins: {
-      type: Array,
-      default: () => []
-    }
-  },
-  setup(props) {
-    const data = {
-      labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
-      datasets: [
-        {
-          backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-          data: [40, 20, 80, 10]
-        }
-      ]
-    };
-
-    const options = {
-      responsive: true,
-      maintainAspectRatio: false
-    };
-
-    return {
-      chartData: data,  // Directly using data
-      chartOptions: options  // Directly using options
-    }
-  }
-})
-
-</script> -->
